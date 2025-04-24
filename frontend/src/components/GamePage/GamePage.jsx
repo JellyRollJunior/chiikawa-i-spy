5cb86ac1c4b4b958e85346444d14061179d0654d
Brandon Lin <brandonlin9716@gmail.com>
2025-04-23 22:26:09 -0700

    Position menu inside image

--- frontend/src/components/GamePage/GamePage.jsx
+++ frontend/src/components/GamePage/GamePage.jsx
@@ -1,20 +1,27 @@
 import { useRef, useState } from 'react';
 import chiikawaWoSagase from '../../assets/chiikawa-wo-sagase.jpg';
+import styles from './GamePage.module.css';
 
 const GamePage = () => {
   const menuRef = useRef(null);
+  const [showMenu, setShowMenu] = useState(false);
 
-  const toggleMenu = () => {
-    menuRef.current.style.display = 'none';
-  }
+  const toggleMenu = (event) => {
+    if (showMenu) {
+      menuRef.current.style.display = 'block';
+    } else {
+      menuRef.current.style.display = 'none';
+    }
+    setShowMenu(!showMenu);
+  };
 
   return (
     <>
       <h1>Game page!</h1>
       <main>
-        <img src={chiikawaWoSagase} alt="" onClick={() => toggleMenu()} />
-        <div ref={menuRef}>
-          I am a menu
+        <div className={styles.imageWrapper}>
+          <img src={chiikawaWoSagase} alt="" onClick={(event) => toggleMenu(event)} />
+          <div className={styles.menu} ref={menuRef}>I am a menu</div>
         </div>
       </main>
     </>
