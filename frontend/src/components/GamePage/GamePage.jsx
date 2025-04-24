63b08cac9db2b4084a8bb816afdbd940c6e848ea
Brandon Lin <brandonlin9716@gmail.com>
2025-04-23 22:14:35 -0700

    Create menu. Remove menu on img click

--- frontend/src/components/GamePage/GamePage.jsx
+++ frontend/src/components/GamePage/GamePage.jsx
@@ -1,10 +1,22 @@
+import { useRef, useState } from 'react';
 import chiikawaWoSagase from '../../assets/chiikawa-wo-sagase.jpg';
 
 const GamePage = () => {
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    menuRef.current.style.display = 'none';
+  }
+
   return (
     <>
       <h1>Game page!</h1>
-      <img src={chiikawaWoSagase} alt="" />
+      <main>
+        <img src={chiikawaWoSagase} alt="" onClick={() => toggleMenu()} />
+        <div ref={menuRef}>
+          I am a menu
+        </div>
+      </main>
     </>
   );
 };
