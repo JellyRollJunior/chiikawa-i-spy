ce1106507b0267923e9427f8f63d1376d2417adf
Brandon Lin <brandonlin9716@gmail.com>
2025-04-24 22:14:03 -0700

    Throw DatabaseErrors on db query failures

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,18 @@ import express from 'express';
 import { gamesRouter } from './routes/gamesRouter.js';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.use('/games', gamesRouter);
 
 app.use((err, req, res, next) => {
     const error = {
+        name: err.name,
         status: err.status ? err.status : 500,
         message: err.message,
-    }
+    };
     res.status(error.status).json(error);
-})
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
--- backend/model/db.js
+++ backend/model/db.js
@@ -1,49 +1,66 @@
 import { PrismaClient } from '@prisma/client';
+import { DatabaseError } from '../errors/DatabaseError.js';
 
 const prisma = new PrismaClient();
 
 const getGames = async () => {
-    const games = await prisma.game.findMany();
-    return games;
+    try {
+        const games = await prisma.game.findMany();
+        return games;
+    } catch (error) {
+        throw new DatabaseError('Error retrieving games', 500);
+    }
 };
 
 const getGame = async (id) => {
-    const game = await prisma.game.findFirst({
-        where: {
-            id: Number(id),
-        },
-        include: {
-            targets: {
-                select: {
-                    id: true,
-                    name: true,
+    try {
+        const game = await prisma.game.findFirst({
+            where: {
+                id: Number(id),
+            },
+            include: {
+                targets: {
+                    select: {
+                        id: true,
+                        name: true,
+                    },
                 },
             },
-        },
-    });
-    return game;
+        });
+        return game;
+    } catch (error) {
+        throw new DatabaseError('Error retrieving game', 500);
+    }
 };
 
 const insertGame = async (name, url) => {
-    const game = await prisma.game.create({
-        data: {
-            name,
-            url,
-        },
-    });
-    return game;
+    try {
+        const game = await prisma.game.create({
+            data: {
+                name,
+                url,
+            },
+        });
+        return game;
+    } catch (error) {
+        throw new DatabaseError('Error inserting game', 500);
+    }
 };
 
 const insertTarget = async (gameId, name, positionX, positionY) => {
-    const target = await prisma.target.create({
-        data: {
-            name,
-            positionX,
-            positionY,
-            gameId,
-        },
-    });
-    return target;
+    try {
+        const target = await prisma.target.create({
+            data: {
+                name,
+                positionX,
+                positionY,
+                gameId,
+            },
+        });
+        return target;
+    } catch (error) {
+        throw new DatabaseError('Error inserting target', 500);
+    }
 };
 
 export { getGames, getGame, insertGame, insertTarget };
