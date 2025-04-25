3216ea1d02e8c5e341c648ad5f23abc17db0677f
Brandon Lin <brandonlin9716@gmail.com>
2025-04-25 16:07:08 -0700

    Pass player variables with getters. Install Jest

--- backend/model/player.js
+++ backend/model/player.js
@@ -1,10 +1,16 @@
 const player = (gameId, targets) => {
+    const gameId = gameId;
     const startTime = new Date().toISOString();
     const targetsNotFound = targets.map((target) => {
         return target.id;
     });
     const targetsFound = [];
 
+    const getGameId = () => gameId;
+    const getStartTime = () => startTime;
+    const getTargetsNotFound = () => targetsNotFound;
+    const getTargetsFound = () => targetsFound;
+    
     const addFoundTarget = (target) => {
         // remove target from not found
         targetsNotFound = targetsNotFound.filter(
@@ -14,7 +20,7 @@ const player = (gameId, targets) => {
         targetsFound.push(target);
     };
 
-    return { gameId, targetsNotFound, targetsFound, startTime, addFoundTarget };
+    return { getGameId, getStartTime, getTargetsNotFound, getTargetsFound, addFoundTarget };
 };
 
 export { player };
