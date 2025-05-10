8af2d95a74dcbc8b2a42d0a7ff9fd75cd102942b
Brandon Lin <brandonlin9716@gmail.com>
2025-05-09 20:48:01 -0700

    Create script to populate targets for chiikawa-village

--- backend/model/scripts/populateTargets.js
+++ backend/model/scripts/populateTargets.js
@@ -1,7 +1,10 @@
 import * as db from '../db.js'
 
 const main = async (gameId) => {
-    await db.insertTarget(gameId, 'Floppy Eared Usagi', 61, 53)
+    await db.insertTarget(gameId, 'Floppy-Eared Usagi looking left', 61, 53);
+    await db.insertTarget(gameId, 'Shisa eating satapanbin~', 96, 22);
+    await db.insertTarget(gameId, 'Hachiware playing guitar', 38, 35);
+    await db.insertTarget(gameId, 'Chiikawa with aura', 62, 23);
 };
 
 const GAME_ID = 1;
