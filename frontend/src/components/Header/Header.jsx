603cf05e82edfb04e919b4671721d1d77491e90f
Brandon Lin <brandonlin9716@gmail.com>
2025-05-11 22:20:43 -0700

    Implement IconWrapper icon selection

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import shared from '../../styles/shared.module.css';
 const Header = ({ children }) => {
   return (
     <header className={styles.header}>
-      <IconWrapper size={32}>
+      <IconWrapper size={38} leftIcon={2} rightIcon={3}>
         <Link to="/">
           <button className={`${styles.headerButton} ${shared.button}`}>
             <h1 className={styles.title}>I Spy Chiikawa!</h1>
--- frontend/src/components/IconWrapper/IconWrapper.jsx
+++ frontend/src/components/IconWrapper/IconWrapper.jsx
@@ -1,18 +1,24 @@
 import styles from './IconWrapper.module.css';
 import chiikawa from '../../assets/characterIcons/chiikawa-icon.png';
 import hachiware from '../../assets/characterIcons/hachiware-icon.png';
+import kurimanju from '../../assets/characterIcons/kurimanju-icon.png';
+import momonga from '../../assets/characterIcons/momonga-icon.png';
+import rakko from '../../assets/characterIcons/rakko-icon.png';
+import usagi from '../../assets/characterIcons/usagi-icon.png';
 
-const IconWrapper = ({ size, children }) => {
+const icons = [chiikawa, hachiware, kurimanju, momonga, rakko, usagi];
+
+const IconWrapper = ({ size, children, leftIcon = 0, rightIcon = 1 }) => {
   return (
     <div className={styles.iconWrapper}>
       <img
-        src={chiikawa}
+        src={icons[leftIcon]}
         style={{ height: size, width: size }}
         alt="Chiikawa icon"
       />
       {children}
       <img
-        src={hachiware}
+        src={icons[rightIcon]}
         style={{ height: size, width: size }}
         alt="Hachiware icon"
       />
