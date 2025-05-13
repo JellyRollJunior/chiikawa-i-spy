949e185e43387273e78a97cf579858259fd5dd76
Brandon Lin <brandonlin9716@gmail.com>
2025-05-13 14:20:52 -0700

    Extract icon data to icons.js

--- frontend/src/components/IconWrapper/IconWrapper.jsx
+++ frontend/src/components/IconWrapper/IconWrapper.jsx
@@ -1,12 +1,5 @@
 import styles from './IconWrapper.module.css';
-import chiikawa from '../../assets/characterIcons/chiikawa-icon.png';
-import hachiware from '../../assets/characterIcons/hachiware-icon.png';
-import kurimanju from '../../assets/characterIcons/kurimanju-icon.png';
-import momonga from '../../assets/characterIcons/momonga-icon.png';
-import rakko from '../../assets/characterIcons/rakko-icon.png';
-import usagi from '../../assets/characterIcons/usagi-icon.png';
-
-const icons = [chiikawa, hachiware, kurimanju, momonga, rakko, usagi];
+import { icons } from './icons.js';
 
 const IconWrapper = ({ children, size, margin = 12, leftIcon = 0, rightIcon = 1 }) => {
   if (leftIcon < 0 || leftIcon >= icons.length) leftIcon = 0;
---
+++ frontend/src/components/IconWrapper/icons.js
@@ -0,0 +1,10 @@
+import chiikawa from '../../assets/characterIcons/chiikawa-icon.png';
+import hachiware from '../../assets/characterIcons/hachiware-icon.png';
+import kurimanju from '../../assets/characterIcons/kurimanju-icon.png';
+import momonga from '../../assets/characterIcons/momonga-icon.png';
+import rakko from '../../assets/characterIcons/rakko-icon.png';
+import usagi from '../../assets/characterIcons/usagi-icon.png';
+
+const icons = [chiikawa, hachiware, kurimanju, momonga, rakko, usagi];
+
+export { icons };
