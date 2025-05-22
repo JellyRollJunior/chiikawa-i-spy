763927805706354a10c0a75edb7387b9692cd20d
Brandon Lin <brandonlin9716@gmail.com>
2025-05-21 21:26:19 -0700

    Array leaderboard stats into labelled columns

--- frontend/src/components/Leaderboard/Leaderboard.jsx
+++ frontend/src/components/Leaderboard/Leaderboard.jsx
@@ -1,15 +1,28 @@
 import { useWinners } from "../../hooks/useWinners";
+import styles from './Leaderboard.module.css';
+import shared from '../../styles/shared.module.css'
 
 const Leaderboard = () => {
   const { winners, loading, error } = useWinners();
 
   return (
     <section>
-      <ul>
-        {winners && winners.map((winner) => (
-          <li>{winner.name}</li>
+      {loading && <h3 className={shared.messages}>Loading winners</h3>}
+      {error && <h3 className={shared.messages}>{error}</h3>}
+      {winners && <ul className={styles.leaderboard}>
+        <li className={styles.leaderboardItem} key={'he was number one!'}>
+          <h4>Place</h4>
+          <h4>Name</h4>
+          <h4>Time</h4>
+        </li>
+        {winners.map((winner, index) => (
+          <li className={styles.leaderboardItem} key={winner.id}>
+            <h4>{index}</h4>
+            <h4>{winner.name}</h4>
+            <h4>{winner.seconds}s</h4>
+          </li>
         ))}
-      </ul>
+      </ul>}
     </section>
   )
 };
