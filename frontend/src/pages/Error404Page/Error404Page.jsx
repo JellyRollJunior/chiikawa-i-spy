1e30b8bf01036166f509cbe5cd2e162197fecbef
Brandon Lin <brandonlin9716@gmail.com>
2025-05-27 15:28:06 -0700

    Style error404Page

--- frontend/src/pages/Error404Page/Error404Page.jsx
+++ frontend/src/pages/Error404Page/Error404Page.jsx
@@ -1,14 +1,37 @@
-import { Footer } from '../../components/Footer/Footer.jsx';
 import { Header } from '../../components/Header/Header.jsx';
+import { IconWrapper } from '../../components/IconWrapper/IconWrapper.jsx';
+import { Link } from 'react-router';
+import { Footer } from '../../components/Footer/Footer.jsx';
 import styles from './Error404Page.module.css';
+import shared from '../../styles/shared.module.css';
+import usagi from '../../assets/error/3-usagi.png';
 
 const Error404Page = () => {
   return (
     <>
       <div>
         <Header />
-        <main>
-          <h2>Error!!!</h2>
+        <main className={shared.marginTopMedium}>
+          <section
+            className={`${shared.card} ${styles.errorWrapper} ${shared.marginTopMedium}`}
+          >
+            <IconWrapper size={36} margin={12} leftIcon={5} rightIcon={5}>
+              <h2>404: Page Not Found</h2>
+            </IconWrapper>
+            <img
+              src={usagi}
+              alt="usagi the ancient wise rabbit looking at you"
+              className={`${styles.errorUsagi} ${shared.marginTopSmall}`}
+            />
+            <p>
+              Ancient wise rabbit once said: <br /> Yaha Yaha unma unma Yaha
+            </p>
+            <Link to="/" className={shared.marginTopSmall}>
+              <button className={shared.primaryButton}>
+                Return to homepage
+              </button>
+            </Link>
+          </section>
         </main>
       </div>
       <Footer />
